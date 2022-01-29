c5ec50624551963b55facb484d9ecce10fa14a2e
Sanensi <william.picard@hotmail.com>
2022-01-29 13:07:13 -0500

    Add mui

--- src/app/Application.tsx
+++ src/app/Application.tsx
@@ -1,4 +1,14 @@
+import styled from "@emotion/styled";
+import { Container } from "@mui/material";
+
+const Red = styled.span`
+  color: red;
+`
 
 export const Application = () => {
-  return <p>Hello World!</p>
+  return (
+    <Container>
+      Hello <Red>World!</Red>
+    </Container>
+  );
 }

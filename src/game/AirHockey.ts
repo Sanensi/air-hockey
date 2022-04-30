22a38325f6776712a1a1955d9eff337fc15d5d87
Sanensi <william.picard@hotmail.com>
2022-04-30 14:55:57 -0400

    Implement basic handle movement

--- src/game/AirHockey.ts
+++ src/game/AirHockey.ts
@@ -1,28 +1,65 @@
-import { Sprite } from "pixi.js";
+import { InteractionEvent, Sprite } from "pixi.js";
 import { PixiApplicationBase } from "../libraries/PixiApplicationBase";
 
 import background_image from "./assets/background.png";
+import handle_image from "./assets/handle.png";
 
 export class AirHockey extends PixiApplicationBase {
   private background = new Sprite();
+
+  private handle1 = new Sprite();
+  private handle1PointerId: number | null = null;
+
+  private handle2 = new Sprite();
+  private handle2PointerId: number | null = null;
+
   public onUpdate = () => { /** noop */ }
 
   public get loading() { return this.app.loader.loading; }
 
   constructor(canvas: HTMLCanvasElement) {
     super(canvas, { antialias: true, backgroundColor: 0xffffff });
-    this.app.loader.add(background_image);
+    this.app.loader.add([background_image, handle_image]);
     this.app.loader.load((_, resources) => {
       this.onUpdate();
       this.background = new Sprite(resources[background_image].texture);
+      this.handle1 = new Sprite(resources[handle_image].texture);
+      this.handle2 = new Sprite(resources[handle_image].texture);
       this.init();
     });
   }
 
   protected start(): void {
     this.background.anchor.set(0.5);
+    this.handle1.anchor.set(0.5);
+    this.handle1.position.set(0, 185 * 2);
+    this.handle2.anchor.set(0.5);
+    this.handle2.position.set(0, -185 * 2);
+
     this.app.stage.addChild(this.background);
+    this.background.addChild(this.handle1);
+    this.background.addChild(this.handle2);
     this.resize();
+
+    this.handle1.interactive = true;
+    this.handle1.addListener("pointerdown", (e: InteractionEvent) => { this.handle1PointerId = e.data.pointerId; });
+    this.handle1.addListener("pointerup", () => { this.handle1PointerId = null; });
+    this.handle1.addListener("pointermove", (e: InteractionEvent) => {
+      if (e.data.pointerId === this.handle1PointerId) {
+        const position = e.data.getLocalPosition(this.background);
+        this.handle1.position.copyFrom(position);
+      }
+    });
+
+    this.handle2.interactive = true;
+    this.handle2.addListener("pointerdown", (e: InteractionEvent) => { this.handle2PointerId = e.data.pointerId; });
+    this.handle2.addListener("pointerup", () => { this.handle2PointerId = null; });
+    this.handle2.addListener("pointermove", (e: InteractionEvent) => {
+      if (e.data.pointerId === this.handle2PointerId) {
+        const position = e.data.getLocalPosition(this.background);
+        this.handle2.position.copyFrom(position);
+      }
+    });
   }
 
   protected resize(): void {
